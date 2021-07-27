a24a7b9a06c28cea7979ef8a09444764ef66b58d
sadiki-dev <sadiki.dev@gmail.com>
2021-07-27 14:55:06 +0200

    install material-ui & Finish the Header & add Linkedin Icons SVG/XML files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets Build a Linkedin Clone</h1>
+      
       {/* Header */}
+      <Header />
 
       {/* App Body */}
         {/* SideBar */}
