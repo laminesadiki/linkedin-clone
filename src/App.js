1d423cb9177e74019fad66b753f5786c033ae254
sadiki-dev <sadiki.dev@gmail.com>
2021-07-29 20:20:56 +0200

    handle states with Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 import Sidebar from './components/Sidebar/Sidebar';
-import { selectUser } from './features/counter/userSlice';
+import { auth } from './configurations/firebase';
+import { login, logout, selectUser } from './features/counter/userSlice';
 
 
 function App() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if(userAuth){
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
 
 
   return (
     <div className="app">
       <Header />
-      {!user ? (<Login />) 
-      :
-        (
+      {!user 
+        ? (<Login />) 
+        : (
           <div className="app__body">
             <Sidebar />
             <Feed />
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { auth } from '../../configurations/firebase';
+import { login } from '../../features/counter/userSlice';
 import "./Login.css"
 
 function Login() {
-    const loginToApp = () => {};
-    const register = () => {};
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    // To put data in data store of Redux
+    const dispatch = useDispatch();
+    
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
+    const register = () => {
+        if(!name)
+            return alert("Please Enter a full name !");
+        auth.createUserWithEmailAndPassword(email,password)
+            .then((userAuth) => {
+                userAuth.user.updateProfile({
+                        displayName: name,
+                        photoURL: profilePic,
+                    })
+                    .then(() => {
+                        dispatch(
+                            login({
+                                email: userAuth.user.email,
+                                uid: userAuth.user.uid,
+                                displayName: name,
+                                photoURL: profilePic,
+                            })
+                        );
+                    });
+            })
+            .catch(error => alert(error));
+    };
 
     return (
         <div className="login">
             <img src="https://logosdownload.com/logo/LinkedIn-logo.svg" alt="" />
             <form>
-                <input placeholder="Full name (required if registering)" type="text" />
-                <input placeholder="Profile pic URL (optional)" type="text" />
-                <input placeholder="Email" type="email" />
-                <input placeholder="Password" type="password" />
+                <input value={name} onChange={e => setName(e.target.value)} placeholder="Full name (required if registering)" type="text" />
+                <input value={profilePic} onChange={e => setProfilePic(e.target.value)}  placeholder="Profile pic URL (optional)" type="text" />
+                <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="email" />
+                <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" />
                 <button type="submit" onClick={loginToApp}>Sign In</button>
             </form>
             <p>Not a member ? {" "}
